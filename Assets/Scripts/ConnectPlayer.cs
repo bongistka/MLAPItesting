c978d60d885b8e5065d698d2c68c87f0a917ca3d
Barbora Darmová <barbora.darmova@virtuplex.cz>
2020-01-15 16:05:53 +0100

    Added SteamVR and networking

---
+++ Assets/Scripts/ConnectPlayer.cs
@@ -0,0 +1,22 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectPlayer : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkingManager.Singleton.StartHost();
+        //GameObject go = Instantiate(playerPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,27 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : NetworkedBehaviour
+{
+    public Transform playerLocal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsLocalPlayer)
+        {
+            playerLocal = GameObject.FindWithTag("FollowHead").transform;
+
+            this.transform.SetParent(playerLocal);
+            this.transform.localPosition = Vector3.zero;
+            this.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
